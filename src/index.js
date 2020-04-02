4a73894984991f5f43587f1fcabfd8c076309c5b
Zohaib Ahmed Butt <zohaibahmedbutt@gmail.com>
2020-04-02 11:36:26 +0300

    Package: Added CORS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 
 app.listen(3000, () =>
   console.log('Adcash NodeJS API listening on port 3000!'),
