c65e187267642f26185818d2827b56614d59aef5
Zohaib Ahmed Butt <zohaibahmedbutt@gmail.com>
2020-04-04 14:58:58 +0300

    Index: Removed redundant code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import express from 'express';
 import cors from 'cors';
-import morgan from 'morgan';
 
 import routes from './config/routes';
-import models, {
-  connectDb,
-  seedData,
-} from './config/database/mongoose';
-import { category, products } from './config/database/seeds';
+import { connectDb } from './config/database/mongoose';
 
 const BASE_API_URL = '/api/v1';
 const CATEGORY_API_URL = `${BASE_API_URL}/categories`;
@@ -20,18 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(CATEGORY_API_URL, routes.CategoriesController);
 
-const eraseDatabaseOnSync = false;
-
 connectDb().then(async () => {
-  if (eraseDatabaseOnSync) {
-    await Promise.all([
-      models.Product.deleteMany({}),
-      models.Category.deleteMany({}),
-    ]);
-
-    seedData(category, products);
-  }
-
   app.listen(3000, () =>
     console.log('Adcash NodeJS API listening on port 3000!'),
   );
